9636e6924a27e357866aec86b054d24702f41031
Valker21 <drjrsm.310731@outlook.com>
2025-07-11 14:01:11 -0500

    Update AssemblyInfo.vb

--- njWorm/njw0rm/My Project/AssemblyInfo.vb
+++ njWorm/njw0rm/My Project/AssemblyInfo.vb
@@ -9,15 +9,21 @@ Imports System.Runtime.InteropServices
 ' Review the values of the assembly attributes
 
 <Assembly: AssemblyTitle("njw0rm by njq8 + Mr.Mobark")> 
+
 <Assembly: AssemblyDescription("njw0rm by njq8 + Mr.Mobark")> 
+
 <Assembly: AssemblyCompany("njq8 + Mr.Mobark")> 
+
 <Assembly: AssemblyProduct("njw0rm")> 
+
 <Assembly: AssemblyCopyright("Copyright © njq8 2013")> 
+
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
+
 <Assembly: Guid("70bd11de-7da1-4a89-b459-8daacc930c20")> 
 
 ' Version information for an assembly consists of the following four values:
@@ -32,4 +38,5 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 <Assembly: AssemblyVersion("3.5.0.0")> 
+
 <Assembly: AssemblyFileVersion("3.5.0.0")> 
