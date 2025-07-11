409598294c9ce7fe5090e8b6ed54e444edb4bfd0
Valker21 <drjrsm.310731@outlook.com>
2025-07-11 14:47:00 -0500

    Update FURL.designer.vb

--- njWorm/njw0rm/FURL.designer.vb
+++ njWorm/njw0rm/FURL.designer.vb
@@ -1,28 +1,43 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class FURL
-    Inherits System.Windows.Forms.Form
 
+Partial Class FURL
+    
+    Inherits System.Windows.Forms.Form
+    
     'Form overrides dispose to clean up the component list.
+    
     <System.Diagnostics.DebuggerNonUserCode()> _
+    
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        
         Try
+            
             If disposing AndAlso components IsNot Nothing Then
+                
                 components.Dispose()
             End If
+            
         Finally
+            
             MyBase.Dispose(disposing)
         End Try
+        
     End Sub
 
     'Required by the Windows Form Designer
+    
     Private components As System.ComponentModel.IContainer
 
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
+    
     <System.Diagnostics.DebuggerStepThrough()> _
+    
     Private Sub InitializeComponent()
+        
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FURL))
+        
         Me.Label1 = New System.Windows.Forms.Label
         Me.Label2 = New System.Windows.Forms.Label
         Me.TextBox1 = New System.Windows.Forms.TextBox
@@ -30,41 +45,51 @@ Partial Class FURL
         Me.Button1 = New System.Windows.Forms.Button
         Me.Button2 = New System.Windows.Forms.Button
         Me.SuspendLayout()
+        
         '
         'Label1
         '
+        
         Me.Label1.AutoSize = True
         Me.Label1.Location = New System.Drawing.Point(1, 3)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(25, 13)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Link"
+        
         '
         'Label2
         '
+        
         Me.Label2.AutoSize = True
         Me.Label2.Location = New System.Drawing.Point(1, 43)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(41, 13)
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "Run As"
+        
         '
         'TextBox1
         '
+        
         Me.TextBox1.Location = New System.Drawing.Point(4, 19)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(324, 20)
         Me.TextBox1.TabIndex = 2
+        
         '
         'TextBox2
         '
+        
         Me.TextBox2.Location = New System.Drawing.Point(4, 59)
         Me.TextBox2.Name = "TextBox2"
         Me.TextBox2.Size = New System.Drawing.Size(140, 20)
         Me.TextBox2.TabIndex = 3
+        
         '
         'Button1
         '
+        
         Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Button1.Location = New System.Drawing.Point(155, 45)
         Me.Button1.Name = "Button1"
@@ -72,9 +97,11 @@ Partial Class FURL
         Me.Button1.TabIndex = 4
         Me.Button1.Text = "Ok"
         Me.Button1.UseVisualStyleBackColor = True
+        
         '
         'Button2
         '
+        
         Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Button2.Location = New System.Drawing.Point(243, 45)
         Me.Button2.Name = "Button2"
@@ -82,9 +109,11 @@ Partial Class FURL
         Me.Button2.TabIndex = 5
         Me.Button2.Text = "Cancel"
         Me.Button2.UseVisualStyleBackColor = True
+        
         '
         'FURL
         '
+        
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Black
@@ -106,10 +135,12 @@ Partial Class FURL
         Me.PerformLayout()
 
     End Sub
+    
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
     Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
     Friend WithEvents Button1 As System.Windows.Forms.Button
     Friend WithEvents Button2 As System.Windows.Forms.Button
+    
 End Class
