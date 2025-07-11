d06955d6cc2bd151d26b97a63792ac20634dce37
Valker21 <drjrsm.310731@outlook.com>
2025-07-11 14:18:41 -0500

    Update About.Designer.vb

--- njWorm/njw0rm/About.Designer.vb
+++ njWorm/njw0rm/About.Designer.vb
@@ -1,37 +1,55 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+
 Partial Class About
+    
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
     <System.Diagnostics.DebuggerNonUserCode()> _
+   
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        
         Try
+            
             If disposing AndAlso components IsNot Nothing Then
+               
                 components.Dispose()
             End If
+            
         Finally
+            
             MyBase.Dispose(disposing)
+            
         End Try
+        
     End Sub
 
     'Required by the Windows Form Designer
+    
     Private components As System.ComponentModel.IContainer
 
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
+   
     Private Sub InitializeComponent()
+        
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(About))
+        
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
+        
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        
         Me.SuspendLayout()
+        
         '
         'PictureBox1
         '
+        
         Me.PictureBox1.Dock = System.Windows.Forms.DockStyle.Left
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
         Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
@@ -40,9 +58,11 @@ Partial Class About
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
+        
         '
         'Label1
         '
+        
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Arial", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.Color.DarkSlateGray
@@ -51,9 +71,11 @@ Partial Class About
         Me.Label1.Size = New System.Drawing.Size(293, 96)
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "___  njw0rm ___" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Version    : 3.4" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Coded By: njq8 + Mr.Mobark" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "______________"
+        
         '
         'Button1
         '
+        
         Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Button1.Font = New System.Drawing.Font("Tahoma", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button1.ForeColor = System.Drawing.Color.Cornsilk
@@ -63,9 +85,11 @@ Partial Class About
         Me.Button1.TabIndex = 3
         Me.Button1.Text = "Mr.Mobark"
         Me.Button1.UseVisualStyleBackColor = True
+        
         '
         'Button2
         '
+        
         Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Button2.Font = New System.Drawing.Font("Tahoma", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button2.ForeColor = System.Drawing.Color.Cornsilk
@@ -75,9 +99,11 @@ Partial Class About
         Me.Button2.TabIndex = 4
         Me.Button2.Text = "@njq8"
         Me.Button2.UseVisualStyleBackColor = True
+        
         '
         'About
         '
+        
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Black
@@ -92,13 +118,17 @@ Partial Class About
         Me.Name = "About"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
         Me.Text = "About"
+        
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+       
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
+    
     Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents Button1 As System.Windows.Forms.Button
     Friend WithEvents Button2 As System.Windows.Forms.Button
+
 End Class
